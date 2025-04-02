3c4cf25dbdbcea7132323be25e7214d26c348e4c
TeamDman <TeamDman9201@gmail.com>
2025-04-02 00:10:04 -0400

    exploring state, trying to find github username lol

---
+++ Add-VSCodeDir.ps1
@@ -0,0 +1,1 @@
+code -a "$Env:APPDATA\Code"
---
+++ examples/recent_workspaces.rs
@@ -0,0 +1,16 @@
+use vscodehelper::storage_json::VSCodeStorageJson;
+
+pub fn main() -> eyre::Result<()> {
+    common::init()?;
+    let storage_json = VSCodeStorageJson::load_from_disk()?;
+    let recent_workspaces = storage_json
+        .profile_associations
+        .workspaces
+        .keys()
+        .collect::<Vec<_>>();
+    println!("You've visited at some point:");
+    for workspace in recent_workspaces.iter().take(5) {
+        println!("  - {workspace}");
+    }
+    Ok(())
+}
