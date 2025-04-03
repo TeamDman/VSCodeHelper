7b5d46161b3d017eb98437c035a7b18bfbf723ff
TeamDman <TeamDman9201@gmail.com>
2025-04-02 23:46:29 -0400

    moving crap around

--- src/state_vscdb/mod.rs
+++ src/state_vscdb/mod.rs
@@ -1,10 +1,49 @@
 pub mod keys;
 pub mod models;
 pub mod schema;
-#[allow(clippy::module_inception)]
-mod state_vscdb;
 
-pub use state_vscdb::VSCodeStateVscdb;
+use crate::state_vscdb::schema::ItemTable::dsl::*;
+use crate::storage_json::paths::VSCodePath;
+use diesel::SqliteConnection;
+use diesel::prelude::*;
+use keys::Key;
+use models::Item;
+use std::path::PathBuf;
+
+pub struct VSCodeStateVscdb {
+    pub connection: SqliteConnection,
+}
+impl VSCodeStateVscdb {
+    pub fn new(connection: SqliteConnection) -> Self {
+        Self { connection }
+    }
+    pub fn try_default() -> eyre::Result<Self> {
+        let vscdb_path: PathBuf = VSCodePath::StateVscdb.try_into()?;
+        let connection = SqliteConnection::establish(&vscdb_path.to_string_lossy())?;
+        Ok(Self { connection })
+    }
+    pub fn keys(&mut self) -> eyre::Result<Vec<String>> {
+        let keys = ItemTable
+            .select(key)
+            .distinct()
+            .load::<String>(&mut self.connection)?;
+        Ok(keys)
+    }
+    pub fn entries(&mut self) -> eyre::Result<Vec<Item>> {
+        let entries = ItemTable
+            .select(Item::as_select())
+            .load::<Item>(&mut self.connection)?;
+        Ok(entries)
+    }
+    pub fn read<K: Key>(&mut self) -> eyre::Result<K::Value> {
+        let item = ItemTable
+            .filter(key.eq(K::KEY))
+            .select(Item::as_select())
+            .first::<Item>(&mut self.connection)?;
+        let rtn = item.parse::<K>()?;
+        Ok(rtn)
+    }
+}
 
 pub mod well_known_keys {
     pub use super::keys::history_recently_opened_paths_list::HistoryRecentlyOpenedPathsListKey;
